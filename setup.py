6bde9f37b33d0bd794c5a7a9f21c2aecb8c8f67e
Elliot Murphy <statik@users.noreply.github.com>
2018-12-07 15:57:19 -0500

    Set up encrypted token for publishing to pypi
    Done by travisCI when pushing tag to github.
    
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ 'numpy', 'scipy', 'pymc3']
+requirements = [ 'numpy', 'scipy', 'pymc3', 'attr']
 
 setup_requirements = ['pytest-runner', ]
 
